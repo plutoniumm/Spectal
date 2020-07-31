a29e3efc4084c4f2e3fd036e99b88cc355ea20ba
Manav Seksaria <ssmaanv@icloud.com>
2020-08-01 02:10:12 +0530

    Changes

--- js/events.js
+++ js/events.js
@@ -1,60 +1,22 @@
 const firebaseConfig = { apiKey: 'AIzaSyDCHqrIu7LTjcElbyTEjkjB3zqN_LMJHFc', authDomain: 'spectal.firebaseapp.com', databaseURL: 'https://spectal.firebaseio.com', projectId: 'spectal', storageBucket: 'spectal.appspot.com', appId: '1:867384801306:web:79c8fa283858b0357ab9a3' }; var app = firebase.initializeApp( firebaseConfig ), evenList = ''; const db = firebase.firestore( app ); const home = db.collection( 'home' ); function getDat ( id ) { return document.getElementById( id ) };
 
-db.collection( 'details' ).doc( 'events' ).get()
-      .then( function ( querySnapshot ) {
-            evenList = querySnapshot.data().list
-            console.log( evenList );
-      } );
-
-function evnDeet ( index = 0 ) {
-      console.log( evenList[ index ] ); x = evenList[ index ];
-      proto = `
-      <div class="container">
-            <div class="row">
-                  <div class="col">
-                        <img src="${x.backimg }">
-                  </div>
-                  <div class="col">
-                        <h1> ${x.name }</h1>
-                        <p> ${x.description }</p>
-                  </div>
-            </div>
-            <div class="row">
-                  <img src="${x.pics[ 1 ] }">
-            </div>
-      </div>
-      `
-      getDat( 'evnDeets' ).style.display = 'block';
-      getDat( 'evnDeets' ).innerHTML = proto;
-      window.location.href = '#evnDeets'
-}
-
 home.doc( 'events' ).get()
       .then( function ( querySnapshot ) {
             list = "";
             querySnapshot.data().types.forEach( ( type, index ) => {
                   list += `
-                  <div class="eventIndi col" onclick="evnDeet(${index })">
-                        <img class="typeImg" src="${type.image }">
-                        <h1 class="onImg">${type.name.slice( 0, 6 ) }</h1>
-                  </div>`
+                  <div class="flip-box">
+                        <div class="flip-box-inner">
+                              <div class="flip-box-front">
+                                    <img src="${type.image }">
+                              </div>
+                              <div class="flip-box-back">
+                                    <p class="w-100">${type.description }</p>
+                              </div>
+                        </div>
+                        <h3>${type.name }</h3>
+                  </div>
+                              `
             } );
-            getDat( 'navi' ).innerHTML += `<div id="evensMain" class="events row">${ list }</div>`;
-            $( document ).ready( function () {
-                  $( '.events' ).slick( {
-                        infinite: true,
-                        slidesToShow: 4,
-                        slidesToScroll: 1,
-                        autoplay: true,
-                        arrows: false,
-                        dots: false,
-                        pauseOnHover: false,
-                        autoplaySpeed: 1000,
-                        responsive: [
-                              { breakpoint: 991, settings: { slidesToShow: 3, } },
-                              { breakpoint: 768, settings: { slidesToShow: 2, } },
-                              { breakpoint: 600, settings: { slidesToShow: 1, } },
-                        ]
-                  } );
-            } );
+            getDat( 'navi' ).innerHTML += `<div id="evensMain" style="display: flex; flex-wrap:wrap;">${ list }</div>`;
       } );
--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function getDat ( id ) { return document.getElementById( id ) }; const firebaseConfig = { apiKey: 'AIzaSyDCHqrIu7LTjcElbyTEjkjB3zqN_LMJHFc', authDomain: 'spectal.firebaseapp.com', databaseURL: 'https://spectal.firebaseio.com', projectId: 'spectal', storageBucket: 'spectal.appspot.com', appId: '1:867384801306:web:79c8fa283858b0357ab9a3' }; var app = firebase.initializeApp( firebaseConfig ), list = "", artsFull; const db = firebase.firestore( app ); const home = db.collection( 'home' );
 
-home.doc( 'about' ).get().then( function ( querySnapshot ) { getDat( 'aboutHead' ).innerHTML = querySnapshot.data().heading; getDat( 'aboutPara' ).innerHTML = querySnapshot.data().paragraph; } );
+home.doc( 'about' ).get().then( function ( q ) { getDat( 'aboutHead' ).innerHTML = q.data().heading; getDat( 'aboutPara' ).innerHTML = q.data().paragraph; } );
 
 home.doc( 'services' ).get()
-      .then( function ( querySnapshot ) {
-            getDat( 'servsHead' ).innerHTML = querySnapshot.data().heading;
-            getDat( 'servsPara' ).innerHTML = querySnapshot.data().paragraph;
+      .then( function ( q ) {
+            getDat( 'servsHead' ).innerHTML = q.data().heading;
+            getDat( 'servsPara' ).innerHTML = q.data().paragraph;
             list = "";
-            querySnapshot.data().services.forEach( service => {
+            q.data().services.forEach( service => {
                   list += `
                   <div class="item">
                         <div class="container">
@@ -28,11 +28,6 @@ home.doc( 'services' ).get()
                                           <div class="row">
                                                 <div class="col">
                                                       <p style="padding-top: 1em; font-size: 100%;" class="movetext">${service.description }</p>
-                                                </div>
-                                          </div>
-                                          <div class="row">
-                                                <div class="col">
-                                                      <button class="artbtn" onclick="window.location.href='../services.html'">Learn More</button>
                                                 </div>
                                           </div>
                                     </div>
@@ -59,90 +54,101 @@ home.doc( 'services' ).get()
             } );
       } );
 
+function artModal ( id ) {
+      home.doc( 'artists' ).collection( 'artists' ).doc( id ).get().then(
+            function ( q ) {
+                  console.log( q.data() );
+            } );
+}
+
 home.doc( 'artists' ).get()
-      .then( function ( querySnapshot ) {
-            artsFull = querySnapshot.data();
+      .then( function ( q ) {
+            artsFull = q.data();
             getDat( 'artsHead' ).innerHTML = artsFull.heading;
             getDat( 'artsPara' ).innerHTML = artsFull.paragraph;
-            list = "";
-            artsFull.artists.forEach( ( artist, index ) => {
-                  list += `
+            home.doc( 'artists' ).collection( 'artists' ).get().then(
+                  function ( querySnapshot ) {
+                        list = "";
+                        querySnapshot.forEach( function ( doc ) {
+                              artist = doc.data();
+                              list += `
                   <div class="item">
                         <div class="artCont">
                               <div class="row" style="margin: 0 auto;">
                                     <div class="col-5">
-                                    <div class="row">
-                                          <div class="col">
-                                                <img src="${artist.image }" />
-                                                <button onclick="artsModal(${index })" type="button" class="artbtn">See More</button>
+                                          <div class="row">
+                                                <div class="col">
+                                                      <img src="${artist.image }" />
+                                                      <button type="button" onclick="artModal('${doc.id }')" class="artbtn blur">See More</button>
+                                                </div>
                                           </div>
                                     </div>
-                                    </div>
-                                    <div class="col-7" style="background-color: rgba(255,255,255,0.15); padding: 1.5em;">
-                                    <div class="row">
-                                          <div class="col">
-                                                <h4 class="movetext">${artist.superhead }</h4>
-                                                <h2 class="movetext">${artist.name }</h2>
+                                    <div class="col-7 blur" style="background-color: rgba(255,255,255,0.15); padding: 1.5em;">
+                                          <div class="row">
+                                                <div class="col">
+                                                      <h2 class="movetext">${artist.name }</h2>
+                                                </div>
                                           </div>
-                                    </div>
-                                    <div class="row">
-                                          <div class="col">
-                                                <p style="padding-top: 1em; font-size: 100%;" class="movetext">${artist.description.slice( 0, 120 ) }...</p>
+                                          <div class="row">
+                                                <div class="col">
+                                                      <p style="padding-top: 1em; font-size: 115%;" class="movetext">${artist.description.slice( 0, 160 ) }...</p>
+                                                </div>
                                           </div>
-                                    </div>
-                                    <div class="row">
-                                          <span class="artSocShow"><i class="fab fa-facebook-f articon"></i></span>
-                                          <span class="artSocShow"><i class="fab fa-instagram articon"></i></span>
-                                          <span class="artSocShow"><i class="fab fa-twitter articon"></i></span>
-                                          <span class="artSocShow"><i class="fab fa-apple articon"></i></span>
-                                          <span class="artSocShow"><i class="fab fa-spotify articon"></i></span>
-                                          <span class="artSocShow"><i class="fab fa-amazon articon"></i></span>
-                                          <span class="artSocShow"><i class="fas fa-globe articon"></i></span>
-                                    </div>
+                                          <div class="row">
+                                                <span class="artSocShow"><i class="fab fa-instagram articon"></i></span>
+                                                <span class="artSocShow"><i class="fab fa-facebook-f articon"></i></span>
+                                                <span class="artSocShow"><i class="fab fa-twitter articon"></i></span>
+                                                <span class="artSocShow"><i class="fas fa-globe articon"></i></span>
+                                                <span class="artSocShow"><i class="fab fa-apple articon"></i></span>
+                                                <span class="artSocShow"><i class="fab fa-spotify articon"></i></span>
+                                                <span class="artSocShow"><i class="fab fa-amazon articon"></i></span>
+                                          </div>
                                     </div>
                               </div>
                         </div>
                   </div>
                   `
-            } );
-            getDat( 'artists' ).innerHTML += `<div id="artsMain" class="artists slider row">${ list }</div>`;
-            $( document ).ready( function () {
-                  $( '.artists' ).slick( {
-                        centerMode: true,
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        autoplay: true,
-                        autoplaySpeed: 2000,
-                        draggable: true,
-                        dots: true,
-                        fade: true,
-                        infinite: true,
-                        cssEase: 'ease',
-                        touchThreshold: 100,
-                        responsive: [
-                              {
-                                    breakpoint: 768,
-                                    settings: {
-                                          arrows: false
-                                    }
-                              },
-                        ]
+                        } );
+                        getDat( 'artists' ).innerHTML += `<div id="artsMain" class="artists slider row">${ list }</div>`;
+                        $( document ).ready( function () {
+                              $( '.artists' ).slick( {
+                                    centerMode: true,
+                                    slidesToShow: 1,
+                                    slidesToScroll: 1,
+                                    autoplay: true,
+                                    autoplaySpeed: 2000,
+                                    draggable: true,
+                                    dots: true,
+                                    arrows: true,
+                                    fade: true,
+                                    infinite: true,
+                                    cssEase: 'ease',
+                                    touchThreshold: 100,
+                                    responsive: [
+                                          {
+                                                breakpoint: 768,
+                                                settings: {
+                                                      arrows: false
+                                                }
+                                          },
+                                    ]
+                              } );
+                        } );
                   } );
-            } );
       } );
 
 home.doc( 'brands' ).get()
-      .then( function ( querySnapshot ) {
-            getDat( 'brandsHead' ).innerHTML = querySnapshot.data().heading;
-            getDat( 'brandsPara' ).innerHTML = querySnapshot.data().paragraph;
+      .then( function ( q ) {
+            getDat( 'brandsHead' ).innerHTML = q.data().heading;
+            getDat( 'brandsPara' ).innerHTML = q.data().paragraph;
             list = "";
-            querySnapshot.data().brands.forEach( brand => {
+            q.data().brands.forEach( brand => {
                   list += `
             <div class="col brnFull">
                   <div class="brnImg" style="background-image: url(${brand.image })">
                   </div>
                   <div class="brnTex d-flex align-items-center text-center">
-                        ${brand.paragraph }
+                        ${brand.description }
                   </div>
                   <button class="brnbtn btn">Learn More</button>
             </div>
@@ -184,11 +190,11 @@ home.doc( 'brands' ).get()
       } );
 
 home.doc( 'events' ).get()
-      .then( function ( querySnapshot ) {
-            getDat( 'evensHead' ).innerHTML = querySnapshot.data().heading;
-            getDat( 'evensPara' ).innerHTML = querySnapshot.data().paragraph;
+      .then( function ( q ) {
+            getDat( 'evensHead' ).innerHTML = q.data().heading;
+            getDat( 'evensPara' ).innerHTML = q.data().paragraph;
             list = "";
-            querySnapshot.data().events.forEach( event => {
+            q.data().events.forEach( event => {
                   list += `
                   <div class="col d-inline-block">
                         <h1><span data-purecounter-end="${event.value }" class="purecounter">0</span></h1>
@@ -202,11 +208,11 @@ home.doc( 'events' ).get()
 
 
 home.doc( 'testimonials' ).get()
-      .then( function ( querySnapshot ) {
-            getDat( 'tmonHead' ).innerHTML = querySnapshot.data().heading;
-            getDat( 'tmonPara' ).innerHTML = querySnapshot.data().paragraph;
+      .then( function ( q ) {
+            getDat( 'tmonHead' ).innerHTML = q.data().heading;
+            getDat( 'tmonPara' ).innerHTML = q.data().paragraph;
             list = "";
-            querySnapshot.data().tmons.forEach( tmon => {
+            q.data().tmons.forEach( tmon => {
                   list += `
                   <div class="col text-center" style="margin-top: 16px;">
                         <p style="padding: 20px;" class"movetext">"${tmon.paragraph }"</p>
@@ -235,10 +241,10 @@ home.doc( 'testimonials' ).get()
 
 
 home.doc( 'contact' ).get()
-      .then( function ( querySnapshot ) {
-            getDat( 'contHead' ).innerHTML = querySnapshot.data().heading + '<br><br>';
+      .then( function ( q ) {
+            getDat( 'contHead' ).innerHTML = q.data().heading + '<br><br>';
             list = "";
-            querySnapshot.data().contacts.forEach( cont => {
+            q.data().contacts.forEach( cont => {
                   list += `
                   <p style="text-align: justify;"> <i class="${cont.icon }" style="padding: 0.5em;"></i> ${ cont.value }</p>
                   `
