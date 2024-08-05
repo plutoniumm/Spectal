c842f821f09866af3a518dcf90c4bca72e499469
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2024-08-05 19:09:46 +0530

    deez

--- src/person.svelte
+++ src/person.svelte
@@ -5,14 +5,14 @@
 <table
   cellspacing="0"
   id={person.n}
-  style="border-collapse:separate;font-family:sans-serif;text-transform:uppercase;font-size:16px;"
+  style="border-collapse:separate;font-family:sans-serif;font-size:14px;"
 >
   <tr>
     <td id="spectalLogo" rowspan="6" style="padding-right:1.5em;">
       <a href="https://www.spectalmanagement.com">
         <img
           height="auto"
-          width="150px"
+          width="95px"
           src="https://i.imgur.com/reiboGk.png"
           alt="Logo"
         />
@@ -22,22 +22,24 @@
   <tr>
     <td
       id="person"
-      style="color:rgb(201,70,60);font-weight:700;font-size:1.25em;min-width: 400px;padding-top:0.5em;line-height:0.8em;"
+      style="color:rgb(201,70,60);font-weight:700;font-size:1.25em;min-width: 275px;padding-top:0.25em;line-height:0.8em;"
       >{person.n}</td
     >
   </tr>
-  <tr style="font-size: 1em;line-height:0.8em;">
-    <td id="number">{person.r}</td>
+  <!-- TITLE -->
+  <tr style="font-size: 1.1em;line-height:1em;">
+    <td id="number" style="padding-top: 0.2em;">{person.r}</td>
   </tr>
-  <tr style="font-size: 0.95em;font-weight:400;">
+  <!-- NUMBER -->
+  <tr style="font-size: 0.8em;font-weight:400;">
     <td id="number" style="padding-top: 0.2em;padding-bottom: 0.25em;">
-      <a style="text-decoration:none;color: #888;" href="tel:{person.nu}"
+      <a style="text-decoration:none;color: #444;" href="tel:{person.nu}"
         >{person.nu}</a
       >
     </td>
   </tr>
   <tr>
-    <td style="padding-bottom:0.5em;">
+    <td>
       <a href="https://linkedin.com/company/spectal-talent-management">
         <img
           height="22px"
@@ -50,7 +52,7 @@
         <img
           height="22px"
           width="22px"
-          src="https://i.imgur.com/8G8uVuC.png"
+          src="https://i.imgur.com/T3XWzex.png"
           alt="Instagram"
         />
       </a>
